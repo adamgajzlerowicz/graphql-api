06cf08420ac8a9f5fb0885c56dcc71f950adf9c3
Adam Gajzlerowicz <adam.gajzlerowicz@performancehorizon.com>
2017-05-18 09:54:15 +0200

    Assotiation is working

--- dupa.spec.js
+++ dupa.spec.js
@@ -2,11 +2,14 @@ import test from 'ava';
 import {schema} from './dupa';
 import {graphql} from 'graphql';
 
-test.cb('Test graphql',  t => {
-    const query = '{ user(id: "1"){firstName}}';
+test.cb('Test graphql', t => {
+
+    const query = '{ user(id: "1"){firstName, company {id}}}';
+
     graphql(schema, query, root).then((response) => {
         console.log(response);
         t.truthy(response);
         t.end();
     });
+
 });
