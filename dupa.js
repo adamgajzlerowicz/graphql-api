d7e7feaa7f88f8c6be0673fa0b10031998c678f8
Adam Gajzlerowicz <adam.gajzlerowicz@performancehorizon.com>
2017-05-23 09:28:52 +0200

    Added mutation to delete and edit

--- dupa.js
+++ dupa.js
@@ -60,8 +60,6 @@ const RootQuery = new GraphQLObjectType({
                     .then(response => response.data)
             }
         }
-
-
     }
 });
 
@@ -76,10 +74,31 @@ const mutation = new GraphQLObjectType({
                 age: {type: new GraphQLNonNull(GraphQLInt)},
                 companyId: {type: GraphQLString}
             },
-            resolve: (parentValue, {firstName, age}) => {
-                return axios.post(`http://localhost:3000/users`, {firstName, age}).then(res => res.data);
+            resolve: (parentValue, {firstName, age, companyId}) => {
+                return axios.post(`http://localhost:3000/users`, {firstName, age, companyId}).then(res => res.data);
             }
-        }
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)}
+            },
+            resolve: (parentValue, {id}) => {
+                return axios.delete(`http://localhost:3000/users/${id}`).then(res => res.data);
+            }
+        },
+        editUser: {
+            type: UserType,
+            args: {
+                id: {type: new GraphQLNonNull(GraphQLString)},
+                firstName: {type: GraphQLString},
+                age: {type: GraphQLInt},
+                companyId: {type: GraphQLString}
+            },
+            resolve: (parentValue, {id, firstName, age, companyId}) => {
+                return axios.patch(`http://localhost:3000/users/${id}`, {firstName, age, companyId}).then(res => res.data);
+            }
+        },
     }
 });
 
